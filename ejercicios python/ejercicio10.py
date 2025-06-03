616a58f1ae0fa94cf4b227c2b7339951b6d89707
PAISANO99 <davidjulian632@gmail.com>
2025-06-03 11:33:47 -0500

    Delete ejercicios python directory

--- ejercicios python/ejercicio10.py
+++
@@ -1,5 +0,0 @@
-# Salario con bonificación
-salario_base = float(input("Ingresa el salario base: "))
-bono = salario_base * 0.10
-salario_total = salario_base + bono
-print("El salario total con bonificación es:", salario_total)
